1cde6e73ffc3a6804f9542ddd51cbbcf5b892347
DavideBarbagrigia <33544830+DavideBarbagrigia@users.noreply.github.com>
2021-06-09 12:59:17 +0200

    Per Giovanni
    Quando aprii l'applicazione ti va direttamente nella pagina del carrello

--- ITicket/photo-gallery/src/app/app-routing.module.ts
+++ ITicket/photo-gallery/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./carrello/carrello.module').then(m => m.CarrelloPageModule)
   },
   {
     path: 'tab2',
