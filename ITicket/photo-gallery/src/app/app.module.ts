544d5b113fdb147f0ab441b174d27c34c84ae72e
DavideBarbagrigia <33544830+DavideBarbagrigia@users.noreply.github.com>
2021-06-16 19:01:48 +0200

    Davide 1
    Davide 1

--- ITicket/photo-gallery/src/app/app.module.ts
+++ ITicket/photo-gallery/src/app/app.module.ts
@@ -9,15 +9,37 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {httpInterceptorProviders} from './interceptors';
+import {IonicStorageModule} from '@ionic/storage';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+      HttpClientModule,
+      IonicModule.forRoot(),
+      /*
+      TranslateModule.forRoot({
+          loader: {
+              provide: TranslateLoader,
+              useFactory: (createTranslateLoader),
+              deps: [HttpClient]
+          }
+      }), */
+      IonicStorageModule.forRoot({
+          name: 'ITicket',
+          driverOrder: ['indexeddb', 'sqlite', 'websql']
+      }),
+      AppRoutingModule],
   providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+      StatusBar,
+      SplashScreen,
+      {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+      httpInterceptorProviders
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ ITicket/photo-gallery/src/app/model/utente.model.ts
@@ -0,0 +1,7 @@
+export class Utente {
+
+    email: string;
+    password: string;    
+
+
+}
