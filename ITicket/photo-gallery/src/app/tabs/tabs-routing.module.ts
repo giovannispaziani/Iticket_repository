e299fd43b49f4160f5d2e0de9f06bb6e9ae1edd4
giovannispaziani <giovanni.spaziani@student.univaq.it>
2021-06-16 17:21:22 +0200

    Carrello

--- ITicket/photo-gallery/src/app/tabs/tabs-routing.module.ts
+++ ITicket/photo-gallery/src/app/tabs/tabs-routing.module.ts
@@ -19,6 +19,12 @@ const routes: Routes = [
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
+
+      {
+        path: 'carrello',
+        loadChildren: () => import('../carrello/carrello.module').then(m => m.CarrelloPageModule)
+      },
+      
       {
         path: '',
         redirectTo: '/tabs/tab1',
