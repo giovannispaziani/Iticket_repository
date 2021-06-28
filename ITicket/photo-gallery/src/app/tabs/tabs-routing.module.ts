1bd48a264442987286ae6bb508c994de0665de30
DavideBarbagrigia <33544830+DavideBarbagrigia@users.noreply.github.com>
2021-06-28 11:04:40 +0200

    ccccc
    xx

--- ITicket/photo-gallery/src/app/tabs/tabs-routing.module.ts
+++ ITicket/photo-gallery/src/app/tabs/tabs-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../registrazione/registrazione.module').then(m => m.RegistrazionePageModule)
       },
       {
         path: 'tab2',
